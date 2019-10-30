eeb4a242b8a8e23cb7bb2ca0e269e7a59c3d10ca
adam <adam.lewispetrie@gmail.com>
2019-10-30 17:34:12 +0000

    mvp complete

--- db/console.rb
+++ db/console.rb
@@ -1,9 +1,41 @@
-require('pg')
+require('pry')
 require_relative('../models/album')
 require_relative('../models/artist')
 
-artist1 = ( {
-  'name' => 'Drake'
-  } )
+Album.delete_all
+Artist.delete_all
 
-  
+
+artist1 = Artist.new( {
+  'name' => 'Drake'
+  });
+  artist1.save()
+
+artist2 = Artist.new( {
+  'name' => 'Kanye West'
+  });
+artist2.save()
+
+album1 = Album.new( {
+  'title' => 'Scorpion',
+  'genre' => 'HipHop',
+  'artist_id' => artist1.id
+  });
+album1.save()
+
+album2 = Album.new( {
+  'title' => 'Views From the 6',
+  'genre' => 'HipHop',
+  'artist_id' => artist1.id
+  });
+album2.save()
+
+album3 = Album.new( {
+  'title' => 'Jesus is King',
+  'genre' => 'Gospel',
+  'artist_id' => artist2.id
+  });
+album3.save 
+
+binding.pry
+  nil
